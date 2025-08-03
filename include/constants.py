b886a6c5414d2ef08a99f0a02536689942e7eec2
DiegoIpaez <diegoipaez2000@gmail.com>
2025-08-03 15:04:10 -0300

    feat: add dbt dependencies and configuration

--- include/constants.py
+++ include/constants.py
@@ -6,3 +6,7 @@ POSTGRES_CONN_ID = "postgres_default"
 # REDIS
 REDIS_PORT = os.getenv("REDIS_PORT")
 REDIS_HOST = os.getenv("REDIS_HOST")
+
+# DBT
+DBT_EXECUTABLE_PATH = f"{os.environ['AIRFLOW_HOME']}/dbt_venv/bin/dbt"
+DBT_NPS_PROJECT_PATH = "/usr/local/airflow/dags/dbt/nba_player_stats"
