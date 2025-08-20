221f66068edff88867991712ed9862c29bb56bcf
DiegoIpaez <diegoipaez2000@gmail.com>
2025-08-20 09:22:29 -0300

    refactor(dbt): match wins have been fixed

--- dags/dbt/nba_player_stats/models/intermediate/int_team_games.sql
+++ dags/dbt/nba_player_stats/models/intermediate/int_team_games.sql
@@ -8,9 +8,10 @@ SELECT
     game_date,
     is_home_game,
     opponent_team_name,
-    COUNT(*) AS total_games_played,
-    COUNT(CASE WHEN game_result = 'W' THEN 1 END) AS games_won,
-    COUNT(CASE WHEN game_result = 'L' THEN 1 END) AS games_lost,
+    game_result,
+    COUNT(*) AS total_players_in_game,
+    MAX(CASE WHEN game_result = 'W' THEN 1 ELSE 0 END) AS game_won,
+    MAX(CASE WHEN game_result = 'L' THEN 1 ELSE 0 END) AS game_lost,
     SUM(points) AS team_points,
     SUM(total_rebounds) AS team_rebounds,
     SUM(assists) AS team_assists,
@@ -18,4 +19,4 @@ SELECT
     SUM(blocks) AS team_blocks,
     SUM(turnovers) AS team_turnovers
 FROM base
-GROUP BY team_name, game_id, season_id, game_date, is_home_game, opponent_team_name
+GROUP BY team_name, game_id, season_id, game_date, is_home_game, opponent_team_name, game_result
--- dags/dbt/nba_player_stats/models/marts/team_season_stats.sql
+++ dags/dbt/nba_player_stats/models/marts/team_season_stats.sql
@@ -6,10 +6,9 @@ WITH team_games AS (
 SELECT
     team_name,
     season_id,
-    SUM(total_games_played) AS games_played,
-    SUM(games_won) AS total_games_won,
-    SUM(games_lost) AS total_games_lost,
-    ROUND(SUM(games_won) * 100.0 / NULLIF(SUM(total_games_played), 0), 2) AS win_percentage,
+    COUNT(*) AS games_played,
+    SUM(game_won) AS total_games_won,
+    SUM(game_lost) AS total_games_lost,
     ROUND(AVG(team_points), 2)AS avg_points_per_game,
     ROUND(AVG(team_rebounds), 2)AS avg_rebounds_per_game,
     ROUND(AVG(team_assists), 2)AS avg_assists_per_game,
