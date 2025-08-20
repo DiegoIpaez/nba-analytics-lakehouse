532f56132746d66ef83034fe38e84c82b5813af7
DiegoIpaez <diegoipaez2000@gmail.com>
2025-08-19 22:21:29 -0300

    feat(dbt): add team season statistics aggregation with performance metrics

---
+++ dags/dbt/nba_player_stats/models/intermediate/int_team_games.sql
@@ -0,0 +1,21 @@
+WITH base AS (
+    SELECT * FROM {{ ref('stg_player_stats') }}
+)
+SELECT
+    player_team_name AS team_name,
+    game_id,
+    season_id,
+    game_date,
+    is_home_game,
+    opponent_team_name,
+    COUNT(*) AS total_games_played,
+    COUNT(CASE WHEN game_result = 'W' THEN 1 END) AS games_won,
+    COUNT(CASE WHEN game_result = 'L' THEN 1 END) AS games_lost,
+    SUM(points) AS team_points,
+    SUM(total_rebounds) AS team_rebounds,
+    SUM(assists) AS team_assists,
+    SUM(steals) AS team_steals,
+    SUM(blocks) AS team_blocks,
+    SUM(turnovers) AS team_turnovers
+FROM base
+GROUP BY team_name, game_id, season_id, game_date, is_home_game, opponent_team_name
